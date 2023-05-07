ca53768a0d6104bb558d448ad162b35effeaac68
Eyob <56386577+ateyob@users.noreply.github.com>
2023-05-08 01:04:54 +0300

    home and about

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     <div className="fixed w-full z-10">
       <nav className="flex items-center justify-between flex-wrap bg-white bg-opacity-90 p-6">
         <div className="flex items-center flex-shrink-0 text-white mr-auto">
-          <img className="h-8 w-auto md:mx-8" src="vercel.svg" alt="Logo" />
+          <img className="h-12 w-auto md:mx-8" src="logo.svg" alt="Logo" />
         </div>
         <div className="block lg:hidden">
           <button onClick={handleToggle} className="flex items-center px-3 py-2 border rounded text-gray-900 border-gray-900 hover:text-white hover:bg-gray-900 hover:border-gray-900">
@@ -23,7 +23,7 @@ const Navbar = () => {
         </div>
         <div className={`${isOpen ? 'block' : 'hidden'} w-full block flex-grow lg:flex lg:items-center lg:w-auto`}>
         <div class="text-sm lg:text-base lg:flex-grow text-right  lg:mr-8 xl:mr-8">
-        <a href="home" class="block mt-4 md:mx-8 lg:inline-block lg:mt-0 text-gray-900 font-bold mr-4">Home</a>
+        <a href="" class="block mt-4 md:mx-8 lg:inline-block lg:mt-0 text-gray-900 font-bold mr-4">Home</a>
         <a href="about" class="block mt-4 md:mx-8 lg:inline-block lg:mt-0 text-gray-900 font-bold mr-4">About Us</a>
         <a href="services" class="block mt-4 md:mx-8 lg:inline-block lg:mt-0 text-gray-900 font-bold mr-4">Services</a>
         <a href="pricing" class="block mt-4 md:mx-8 lg:inline-block lg:mt-0 text-gray-900 font-bold mr-4">Pricing</a>
--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,63 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import {GiTakeMyMoney} from 'react-icons/gi'
-import {FaRegHandshake} from 'react-icons/fa'
-import {BsGlobe} from 'react-icons/bs'
-import Navbar from '@/components/Navbar'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { GiTakeMyMoney } from 'react-icons/gi';
+import { FaRegHandshake } from 'react-icons/fa';
+import { BsGlobe } from 'react-icons/bs';
+import Navbar from '@/components/Navbar';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <section>
-  <Navbar />
+    <section className="bg-gray-100">
+      <Navbar />
 
-  <div className="flex flex-col md:flex-row h-screen">
-    <div className="h-screen md:w-1/2 bg-[#4713AD] relative">
-      <div className="p-4 md:px-16 md:py-64 py-32">
-        <div className="text-3xl md:text-4xl text-white leading-tight font-bold">
-          HIRE THE BEST <br/> <span className="text-[#FEBF0E]">TELEMARKETERS</span>
+  <div className="flex flex-col md:flex-row items-center justify-between h-screen max-w-8xl mx-auto px-4">
+    <div className="md:w-1/2 bg-[#4713AD] py-20 px-6 md:px-9 h-full flex flex-col items-start text-white">
+        
+      <div className="flex flex-col h-screen justify-between items-center">
+        <div className="flex-grow flex-shrink">
+          <div className='flex flex-col items-center'>
+            <div className='flex flex-col items-start gap-6'>
+              <h1 className="text-3xl md:text-4xl font-bold leading-tight mt-0 pt-32">
+                HIRE THE BEST <br/> <span className="text-[#FEBF0E]">TELEMARKETERS</span>
+              </h1>
+
+              <p className="text-base text-gray-300 my-2 max-w-[90%] text-left">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mauris nisi. Etiam consequat, nulla in semper semper,
+                turpis ligula commodo sapien, eget tempus arcu sem non mauris.
+              </p>
+
+              <button className="bg-[#FEBF0E] hover:bg-yellow-500 text-white font-semibold py-2 px-8 rounded transition-all duration-300 my-2">
+                Read More
+              </button>
+            </div>
+          </div>
         </div>
 
-        <div className="mt-4 md:mt-8 w-full md:w-[70%]">
-          <p className="text-sm md:text-base text-gray-300 mb-8">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Sed ac mauris nisi. Etiam consequat, nulla in semper semper,
-            turpis ligula commodo sapien, eget tempus arcu sem non mauris.
-          </p>
-
-          <button className="bg-[#FEBF0E] hover:bg-yellow-500 text-white font-semibold py-2 px-4 rounded">
-            Read More
-          </button>
-        </div>
-      </div>
-
-      <div className="absolute bottom-1 left-4 md:left-16 p-2 md:p-4 flex gap-4 md:gap-6">
-        <div className="bg-[#FEBF0E] w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 rounded-full flex justify-center items-center">
-          <GiTakeMyMoney className="text-white" size={20} />
-        </div>
-
-        <div className="bg-[#FEBF0E] w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 rounded-full flex justify-center items-center">
-          <FaRegHandshake className="text-white" size={20} />
-        </div>
-
-        <div className="bg-[#FEBF0E] w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 rounded-full flex justify-center items-center">
-          <BsGlobe className="text-white" size={20} />
+        <div className="flex items-center justify-center">
+          <div className="rounded-full h-12 w-12 bg-[#FEBF0E] flex items-center justify-center mr-4">
+            <GiTakeMyMoney className="text-white text-2xl" />
+          </div>
+          <div className="rounded-full h-12 w-12 bg-[#FEBF0E] flex items-center justify-center mr-4">
+            <FaRegHandshake className="text-white text-2xl" />
+          </div>
+          <div className="rounded-full h-12 w-12 bg-[#FEBF0E] flex items-center justify-center">
+            <BsGlobe className="text-white text-2xl" />
+          </div>
         </div>
       </div>
     </div>
+    
+
+        <div className="md:w-1/2 bg-[url('https://res.cloudinary.com/duthzbfub/image/upload/v1683267339/telemarketing%20appointments/hero-min_aomggp.png')] bg-no-repeat bg-center bg-fixed bg-cover w-full h-full hidden md:block"></div>
+      </div>
+
+      <div class="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#FEBF0E] rounded-full flex items-center justify-center z-10 hidden md:flex md:h-24 w-24 lg:h-36 lg:w-36">
+          <img src="/assets/Mask.png" alt="[IMAGE ALT TEXT]" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full p-2" />
+          {/* <div class="border-l-2 border-black absolute right-1/2 h-full transform translate-y-1/2"></div> */}
+      </div>
       
-    <div className="md:w-1/2 bg-right bg-cover bg-no-repeat" style={{backgroundImage: 'url("https://res.cloudinary.com/duthzbfub/image/upload/v1683267339/telemarketing%20appointments/hero-min_aomggp.png")'}}></div>
-  </div>
-</section>
-
-
-  )
+    </section>
+  );
 }
