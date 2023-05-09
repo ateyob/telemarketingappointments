1d91fbdf3d3625767db7f4fe8a6710fdece81b6d
Eyob <ateyob@gmail.com>
2023-05-09 17:19:28 +0300

    team started

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,6 @@
 import Navbar from '@/components/Navbar'
+import { teamInfo } from '@/utils/constants'
+import Image from 'next/image'
 import React from 'react'
 
 const about = () => {
@@ -69,14 +71,21 @@ const about = () => {
 
     <section className="w-screen h-screen md:pb-[150px] flex flex-col bg-white   ">
         <div className='h-1/2 flex'>
-            <div class='bg-[#4713AD] w-1/2 relative rounded-br-[160px]'>
-                <img src="https://res.cloudinary.com/duthzbfub/image/upload/v1683267261/telemarketing%20appointments/team1-min_auy4p8.png" alt="[IMAGE ALT TEXT]" class="rounded-full absolute bottom-0 right-0 border-8 border-yellow-400 h-80 w-80 object-cover" />
+            <div class='bg-[#4713AD] w-1/2 h-3/4 md:h-full relative rounded-br-[75px] md:rounded-br-[142px] lg:rounded-br-[160px]'>
+                <img src="https://res.cloudinary.com/duthzbfub/image/upload/v1683267261/telemarketing%20appointments/team1-min_auy4p8.png" alt="[IMAGE ALT TEXT]" 
+                class="rounded-full absolute
+                 bottom-0 right-0 border-4 md:border-8
+                 border-yellow-400 
+                 h-32 w-32
+                md:h-72 md:w-72
+                 lg:h-80 lg:w-80 object-cover
+                " />
             </div>
 
-                <div className=' w-1/2 px-12 py-16 bg-white text-black'>
-                    <div className='text-lg md:text-6xl lg:text-7xl font-normal'>
+                <div className=' w-1/2 px-4 md:px-12 py-16 bg-white text-black'>
+                    <div className='text-2xl md:text-5xl lg:text-7xl font-normal'>
                     <span className=' underline decoration-[#FEBF0E]'> Our </span> Best <span className=' text-[#FEBF0E]'>Team</span>
-                        <p className=' text-sm md:text-md lg:text-lg pt-8'>
+                        <p className=' text-xs md:text-md lg:text-lg pt-8'>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                             Sed ac mauris nisi. Etiam consequat, nulla in semper semper,
                             turpiLorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -85,7 +94,7 @@ const about = () => {
                 </div>
             </div>
 
-            {/* <div class="h-1/2 flex justify-center items-center px-72">
+            {/* <div class="h-1/2 grid grid-cols-2 md:grid md:grid-cols-4 justify-center items-center px-4">
                 <div class="flex flex-col items-center mx-4">
                     <div class="rounded-full border-4 border-yellow-400 h-32 w-32 object-cover"></div>
                         <h2 class="text-black font-bold text-lg mt-4">John Doe</h2>
@@ -114,12 +123,30 @@ const about = () => {
                             <p class="text-black mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nibh justo, dignissim non luctus ac.</p>
                 </div>
     
-            </div> */}
-    
+            </div>
+     */}
 
+            <section className="w-screen px-4 md:px-5 pt-[50px] pb-[50px] md:pt-[100px] md:pb-[150px]">
+                <div className="max-w-[1120px] mx-auto">
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 mobile:gap-x-7 gap-y-7 mt-8 mobile:mt-[70px]">
+                        {teamInfo.map((member, index) => (
+                            <div key={index} className="flex flex-col hover:scale-110 transition-all ease-in-out duration-300 items-center">
+                                <div className="h-[300px] mobile:h-[330px] w-full" style={{ background: member.color }}>
+                                    <Image src={member.img} height={100} width={100} alt="team member" className="w-full h-full object-cover" />
+                                </div>
+                                <span className="text-[16px] text-black mobile:text-[20px] mt-3 mobile:mt-5">{member.name}</span>
+                                <span className="text-[14px]" style={{ color: member.color }}>
+                                    {member.email}
+                                </span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
 
 
     </section>
+
 
     </div>
 
---
+++ utils/constants.js
@@ -0,0 +1,28 @@
+import ima from '@/public/assets/Mask.png'
+
+export const teamInfo = [
+    {
+        name: 'Eyob Kefyalew, CEO',
+        email: 'eyob@kefyalew.com',
+        img: ima,
+        color: '#FF006C',
+    },
+    {
+        name: 'Samuel Dereje, Top G',
+        email: 'samuel@hello.com',
+        img: ima,
+        color: '#004EFF',
+    },
+    {
+        name: 'Tuna, Cat',
+        email: 'hello@hello.com',
+        img: ima,
+        color: '#00FFCE',
+    },
+    {
+        name: 'Nati, Dog',
+        email: 'natnael@hello.com',
+        img: ima,
+        color: '#FF7600',
+    },
+];
